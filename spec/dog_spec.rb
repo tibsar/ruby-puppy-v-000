6cd51df24a6bc220ab59c9b933e9d199696edff5
Sara Tibbetts <sxt4866@rit.edu>
2018-02-16 01:25:14 +0000

    automatically backed up by learn

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -25,6 +25,7 @@ describe "Dog" do
 
   describe ".all" do
     it "is a class method that puts out the name of each dog to the terminal" do
+      binding.pry
       expect{Dog.all}.to output("Pluto\nFido\nMaddy\n").to_stdout
     end
   end
